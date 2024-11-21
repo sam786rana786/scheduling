5c9a9b1fcfb6db011cfb1c261f9897b584df274b
Munish Rana <munishmr2007@gmail.com>
2024-11-21 17:39:57 +0000

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white flex justify-center px-4 h-16 items-center">
+        <span className="">Copyright &copy; All rights reserved</span>
+    </footer>
+  )
+}
+
+export default Footer
